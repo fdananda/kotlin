7d26136a29efd855ae8d6f2b8445df90ac0db51f
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-01 06:13:50 -0800

    MainActivity.kt

---
+++ personalizacao_cores_noactionbar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fdananda.kotlinpersonalizaocoresbarrasuperior
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
