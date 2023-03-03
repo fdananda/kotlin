58a8ba893b2628777becd6238fc610b72c0c261c
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-03 09:22:07 -0800

    Atributo.kt

---
+++ crud_sharedpreferences_com_objeto/Atributo.kt
@@ -0,0 +1,35 @@
+package com.fdananda.kotlincrudsharedpreferencescomobjeto
+
+class Atributo {
+
+    private var atributo1: String? = null
+    private var atributo2: String? = null
+    private var atributo3: String? = null
+
+    constructor() {}
+
+    fun getAtributo1(): String? {
+        return atributo1
+    }
+
+    fun setAtributo1(atributo1: String?) {
+        this.atributo1 = atributo1
+    }
+
+    fun getAtributo2(): String? {
+        return atributo2
+    }
+
+    fun setAtributo2(atributo2: String?) {
+        this.atributo2 = atributo2
+    }
+
+    fun getAtributo3(): String? {
+        return atributo3
+    }
+
+    fun setAtributo3(atributo3: String?) {
+        this.atributo3 = atributo3
+    }
+
+}
