c615a3a733ea04c69ce8d99cfd010c4185cc5df1
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-05 07:09:31 -0800

    AdapterCustomizado.kt

---
+++ crud_sqllite_dao/AdapterCustomizado.kt
@@ -0,0 +1,33 @@
+package com.fdananda.kotlincrudsqlitedao
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.annotation.Nullable
+import java.lang.String
+import kotlin.Int
+
+
+class AdapterCustomizado(context: Context, atributos: ArrayList<Atributo?>?) :
+    ArrayAdapter<Atributo?>(context, R.layout.adapter, atributos as List<Atributo?>) {
+    //val context: Context
+    val atributos: ArrayList<Atributo?>? = atributos
+
+
+    override fun getView(position: Int, @Nullable convertView: View?, parent: ViewGroup): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val viewPersonalizada: View = inflater.inflate(R.layout.adapter, parent, false)
+        val id = viewPersonalizada.findViewById(R.id.textViewId) as TextView
+        val atributo1 = viewPersonalizada.findViewById(R.id.textViewAtributo1) as TextView
+        val atributo2 = viewPersonalizada.findViewById(R.id.textViewAtributo2) as TextView
+        val atributo3 = viewPersonalizada.findViewById(R.id.textViewAtributo3) as TextView
+        atributo1.text = atributos!![position]!!.getAtributo1()
+        atributo2.text = atributos[position]!!.getAtributo2()
+        atributo3.text = atributos[position]!!.getAtributo3()
+        id.text = String.valueOf(atributos[position]!!.getId())
+        return viewPersonalizada
+    }
+}
