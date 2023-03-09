e87688c46d21035fac122d3dec470aab20d83009
Fernanda Moreira (fdananda) <67290766+fdananda@users.noreply.github.com>
2023-03-09 08:38:41 -0800

    MainActivity.kt

---
+++ mvvm_com_fragment/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fdananda.kotlinviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fdananda.kotlinviewmodel.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
